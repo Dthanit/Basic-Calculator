853ab1696f6d46a486c4c9b4cb93fd40fde626f9
Dthanit <43135317+Dthanit@users.noreply.github.com>
2018-11-07 22:13:02 +0700

    Update BasicCalculator.java

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 public class BasicCalculator
 {
-     
-      public static void main(String[] args)
+     public static void main(String[] args)
       {  
           Scanner sc = new Scanner(System.in);
           System.out.print("Enter x = ");
